d6010b9b575b537cfb02927890a30aa6893e82be
henriquecrono <henriquemiranda5@hotmail.com>
2023-07-12 18:05:30 -0300

    add middleware to validate create task body

---
+++ server/src/middleware/task/index.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express';
+
+import { ITaskRequest } from '../../interfaces/task';
+
+const validateCreateTaskBody = (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	const reqBody: ITaskRequest = req.body;
+	const title = reqBody.title;
+
+	if (!title) {
+		return res.status(400).json({
+			message: 'Field "title" is required',
+		});
+	}
+
+	next();
+};
+
+export { validateCreateTaskBody };
--- server/src/router.ts
+++ server/src/router.ts
@@ -2,9 +2,11 @@ import express from 'express';
 
 import { createTask, listTasks } from './controllers/task';
 
+import { validateCreateTaskBody } from './middleware/task';
+
 const router = express.Router();
 
 router.get('/task', listTasks);
-router.post('/task', createTask);
+router.post('/task', validateCreateTaskBody, createTask);
 
 export default router;
