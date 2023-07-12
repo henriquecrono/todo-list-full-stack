63e5104cc054262be48c56d85d10b7ce04421f99
henriquecrono <henriquemiranda5@hotmail.com>
2023-07-12 17:05:16 -0300

    fix connection envs

--- server/src/models/connection.ts
+++ server/src/models/connection.ts
@@ -1,5 +1,9 @@
 import mysql from 'mysql2/promise';
 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const connection = mysql.createPool({
 	host: process.env.MYSQL_HOST,
 	user: process.env.MYSQL_USERNAME,
