aacbb712437bbd23ca5f658317c38c2c1160fe7d
henriquecrono <henriquemiranda5@hotmail.com>
2023-07-12 17:49:51 -0300

    add model to create task

--- server/src/models/task/index.ts
+++ server/src/models/task/index.ts
@@ -1,8 +1,20 @@
 import connection from '../connection';
 
+import { ITaskCreate } from '../../interfaces/task';
+
 const list = async () => {
 	const [tasks] = await connection.execute('SELECT * FROM tasks');
 	return tasks;
 };
 
-export { list };
+const create = async (task: ITaskCreate) => {
+	const { title, status, createdAt } = task;
+
+	const query = `INSERT INTO tasks(title, status, createdAt) VALUES ('${title}', '${status}', '${createdAt}')`;
+
+	const [createdTask] = await connection.execute(query);
+
+	return createdTask;
+};
+
+export { list, create };
